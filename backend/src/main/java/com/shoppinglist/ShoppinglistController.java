84e8e035e94b0dff05d5e05a834311a45856132c
JavaGerben <g.j.mertens@outlook.com>
2024-05-30 10:48:46 +0200

    backend works

---
+++ backend/src/main/java/com/shoppinglist/ShoppinglistController.java
@@ -0,0 +1,20 @@
+package com.shoppinglist;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/shopping-list-items")
+public class ShoppinglistController {
+    @GetMapping
+    public List<String> get() {
+        return List.of(
+                "Boter",
+                "Kaas",
+                "Eieren"
+        );
+    }
+}
